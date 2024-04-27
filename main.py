a57a05e7e2ded0eff2f71bbd56429525b2bd46e4
Brighton Weekes <brightonweekes22@gmail.com>
2024-04-27 16:55:14 -0700

    added basic gui

--- main.py
+++ main.py
@@ -1,6 +1,14 @@
 import random
 import pygame
 
+# Define global variables
+fps = 60
+backdrop_color = '#325320'
+player_count = 2
+current_player = 0
+starting_tile_count = 14
+display_resolution = (screen_width, screen_height) = (1600, 900)
+
 
 # Define the Tile class and give it color and value attributes
 class Tile:
@@ -8,6 +16,43 @@ class Tile:
         self.color = color
         self.value = value
         on_board = False
+
+
+# Define the Player class and assign functions
+class Player:
+    def __init__(self):
+        self.hand = []
+        self.hand_backup = []
+        self.has_placed = False     # Store whether the player has placed their initial sets adding to 30 or greater
+
+    def draw(self):
+        self.hand.append(deck[0])
+        deck.pop(0)
+
+    def place(self):
+        pass
+
+
+def resize_screen():
+    global tile_holder
+    tile_holder = pygame.transform.scale(tile_holder, (screen_width*.8, screen_height*.15))
+
+
+# Main game turn logic
+def main():
+    pass
+    
+
+# Check if current board state is correct
+def check_board():
+    valid_board = False
+
+    return valid_board
+
+
+def pass_turn():
+    global current_player
+    current_player = (current_player+1) % player_count
 
 
 # Define the deck and fill it with appropiate tiles
@@ -22,43 +67,28 @@ for i in range(2):
 
 random.shuffle(deck)
 
-# Define other variables
-fps = 60
-backdrop_color = '#325320'
-player_count = 2
-starting_tile_count = 14
-
-
-# Define the Player class and assign functions
-class Player:
-    def __init__(self):
-        self.hand = []
-        self.has_placed = False     # Store whether the player has placed their initial sets adding to 30 or greater
-
-    def draw(self):
-        self.hand.append(deck[0])
-        deck.pop(0)
-
-    def place(self):
-        pass
-
-
-# Main game turn logic
-def main():
-    pass
-
-
 # Backend setup
-board = []      # List containing all lists of tiles on board
-players = []    # List containing all Player objects
+board = []              # List containing all lists of tiles on board
+board_backup = []       # Stores board as it was in beginning of turn
+ui_board = []           # Stores coordinate locations for each tile displayed on the board
+ui_board_backup = []    # Stores ui board as it was in beginning of turn
+players = []            # List containing all Player objects
 for player in range(player_count):
     players.append(Player())
     for starting_tile in range(starting_tile_count):
         players[player].draw()
 
+# Load UI Elements
+tile_holder = pygame.image.load('./uiImages/tile_holder.png')
+# done button
+
+
+
+resize_screen()
+
 # pygame setup
 pygame.init()
-screen = pygame.display.set_mode((1280, 720))
+screen = pygame.display.set_mode(display_resolution)
 clock = pygame.time.Clock()
 running = True
 
@@ -68,8 +98,8 @@ while running:
             running = False
 
     screen.fill(backdrop_color)
+    screen.blit(tile_holder, (screen_width/2-tile_holder.get_width()/2, screen_height-tile_holder.get_height()))
 
-    # RENDER YOUR GAME HERE
     main()
 
     # flip() the display to put your work on screen
